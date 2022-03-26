25ea7f880c23c95be2665601f8ede103026f4e46
marcoamolinaro <molinaromarcoaurelio@gmail.com>
2022-03-25 22:36:32 -0300

    Endpoints findall and by id

--- backend_avaliacao/src/main/java/com/newchallenge/avaliacao/services/ImovelService.java
+++ backend_avaliacao/src/main/java/com/newchallenge/avaliacao/services/ImovelService.java
@@ -3,12 +3,14 @@ package com.newchallenge.avaliacao.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.newchallenge.avaliacao.dto.ImovelDTO;
 import com.newchallenge.avaliacao.entities.Imovel;
 import com.newchallenge.avaliacao.repositories.ImovelRepository;
 
+@Service
 public class ImovelService {
 
 	@Autowired
@@ -21,5 +23,14 @@ public class ImovelService {
 		Page<ImovelDTO> page = result.map(x -> new ImovelDTO(x));
 		
 		return page;
+	}
+	
+	@Transactional(readOnly = true)
+	public ImovelDTO findById(Long id) {
+		Imovel result = repository.findById(id).get();
+		
+		ImovelDTO dto = new ImovelDTO(result);
+		
+		return dto;
 	}
 }
